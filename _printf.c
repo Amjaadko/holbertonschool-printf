4e56bb87da1e733d453154846d083d9db7da341d
Amjad Alomani <iamjaadk@gmail.com>
2025-09-29 13:13:42 +0000

    Task 0: implemented _printf with %c %s and %%

--- _printf.c
+++ _printf.c
@@ -1,42 +1,57 @@
 #include "main.h"
 
 /**
+ * handle_specifier - Handles a single format specifier
+ * @spec: the specifier character
+ * @args: va_list containing the arguments
+ *
+ * Return: number of characters printed
+ */
+int handle_specifier(char spec, va_list args)
+{
+	if (spec == 'c')
+		return (print_char(args));
+	if (spec == 's')
+		return (print_string(args));
+	if (spec == '%')
+	{
+		write(1, "%", 1);
+		return (1);
+	}
+
+	/* unsupported specifier: print '%' and the character */
+	write(1, "%", 1);
+	write(1, &spec, 1);
+	return (2);
+}
+
+/**
  * _printf - Custom printf function
  * @format: format string
  *
- * Return: number of characters printed, or -1 if error
+ * Return: number of characters printed, or -1 on error
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
 
-	if (format == NULL) /* ✅ شرط لو الفورمات NULL */
+	if (format == NULL)
 		return (-1);
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format[i])
 	{
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
 			i++;
-			if (format[i] == 'c')
-				count += print_char(args);
-			else if (format[i] == 's')
-				count += print_string(args);
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-			else
-			{
-				/* لو specifier غير مدعوم: اطبعي % والحرف نفسه */
-				write(1, "%", 1);
-				write(1, &format[i], 1);
-				count += 2;
-			}
+			count += handle_specifier(format[i], args);
 		}
 		else
 		{

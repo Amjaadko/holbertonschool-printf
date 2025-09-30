f08b1caa69945286306f792f1102d3a744e488b7
noura398 <0nourasubaie@gmail.com>
2025-09-30 13:55:16 +0000

    fixing task 0 cause it showes me an error when i try to run the tests

--- _printf.c
+++ _printf.c
@@ -1,40 +1,105 @@
 #include "main.h"
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - custom printf function
+ * put_char - writes one character to stdout
+ * @c: character to write
+ *
+ * Return: 1 on success, -1 on error
+ */
+static int put_char(char c)
+{
+	if (write(1, &c, 1) == -1)
+		return (-1);
+	return (1);
+}
+
+/**
+ * put_str - writes a C-string to stdout
+ * @s: string to write; if NULL, prints "(null)"
+ *
+ * Return: number of characters written, or -1 on error
+ */
+static int put_str(const char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		s = "(null)";
+
+	while (s[i] != '\0')
+	{
+		if (write(1, &s[i], 1) == -1)
+			return (-1);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _printf - produces output according to a format
  * @format: format string
- * Return: number of characters printed
+ *
+ * Supported specifiers: %c, %s, %%.
+ * For any other specifier (e.g. %K, %!), prints '%' then the character.
+ *
+ * Return: number of characters printed, or -1 on error
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
+	va_list ap;
+	int count = 0;
+	int tmp;
+	int i = 0;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			if (put_char(format[i]) == -1)
+				return (va_end(ap), -1);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == '\0')
+				return (va_end(ap), -1);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-                return (-1);
-
-            count += handle_specifier(format[i], args);
-        }
-        else
-        {
-            _putchar(format[i]);
-            count++;
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+			if (format[i] == 'c')
+			{
+				if (put_char((char)va_arg(ap, int)) == -1)
+					return (va_end(ap), -1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				tmp = put_str(va_arg(ap, char *));
+				if (tmp == -1)
+					return (va_end(ap), -1);
+				count += tmp;
+			}
+			else if (format[i] == '%')
+			{
+				if (put_char('%') == -1)
+					return (va_end(ap), -1);
+				count++;
+			}
+			else
+			{
+				/* Unknown specifier: print '%' then the char */
+				if (put_char('%') == -1 || put_char(format[i]) == -1)
+					return (va_end(ap), -1);
+				count += 2;
+			}
+		}
+		i++;
+	}
+	va_end(ap);
+	return (count);
 }
-
--- main.h
+++ main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-
 int _printf(const char *format, ...);
-int _putchar(char c);
-int handle_specifier(char c, va_list args);
-int print_number(int n);
 
-#endif
-
+#endif /* MAIN_H */

e7038a9cc9e686cfd0b6cbde34b47930150e2788
noura398 <0nourasubaie@gmail.com>
2025-09-30 18:18:39 +0300

    Update print_numbers.c

--- print_numbers.c
+++ print_numbers.c
@@ -1,40 +1,48 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
+ * print_unsigned_recursive - prints an unsigned long number
  * @n: number to print
  *
- * Return: number of characters printed, or -1 on error
+ * Return: number of chars printed, or -1 on error
+ */
+static int print_unsigned_recursive(unsigned long n)
+{
+	int count = 0, r;
+
+	if (n / 10)
+	{
+		r = print_unsigned_recursive(n / 10);
+		if (r == -1)
+			return (-1);
+		count += r;
+	}
+	r = _putchar((char)('0' + (n % 10)));
+	if (r == -1)
+		return (-1);
+	return (count + r);
+}
+
+/**
+ * print_number - prints a signed integer (handles INT_MIN safely)
+ * @n: number to print
+ *
+ * Return: number of chars printed, or -1 on error
  */
 int print_number(int n)
 {
-	int count = 0;
-	int r;
+	long num = n;    /* widen to handle INT_MIN */
+	int count = 0, r;
 
-	if (n < 0)
+	if (num < 0)
 	{
 		r = _putchar('-');
 		if (r == -1)
 			return (-1);
 		count += r;
-		/* careful with INT_MIN */
-		if (n / 10 != 0)
-			count += print_number(-(n / 10));
-		r = _putchar('0' + (-(n % 10)));
-		if (r == -1)
-			return (-1);
-		return (count + r);
+		num = -num;
 	}
-
-	if (n / 10)
-	{
-		r = print_number(n / 10);
-		if (r == -1)
-			return (-1);
-		count += r;
-	}
-
-	r = _putchar('0' + (n % 10));
+	r = print_unsigned_recursive((unsigned long)num);
 	if (r == -1)
 		return (-1);
 	return (count + r);
